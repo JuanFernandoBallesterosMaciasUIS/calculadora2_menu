b957e353343f208d8387ab9d34b26d0ea22951b4
juanfernando <https://github.com/JuanFernandoBallesterosMaciasUIS>
2022-09-10 08:29:16 -0500

    Se agrego procesamiento en el programa

--- calculadora_menu.py
+++ calculadora_menu.py
@@ -17,8 +17,42 @@ print("1. Sumar (El primero más el segundo)")
 print("2. Restar (El primero menos el segundo)")
 print("3. Multiplicar (El primero por el segundo)")
 print("4. Dividir (El primero sobre el segundo)")
-print("5. Potencia (El primero á la potencia del segundo)")
+print("5. Potencia (El primero a la potencia del segundo)")
 print("6. Logaritmo (El logaritmo del primero)")
 
 opcion = int(input("\nDame la opción: "))
 
+#processing
+if (opcion == 1):
+    z = x + y
+    print(x," + ", y)
+elif (opcion == 2):
+    z = x - y
+    print(x,"-",y)
+elif (opcion == 3):
+    z = x * y
+    print(x,"*",y)
+elif (opcion == 4 and y != 0):
+    z = x / y
+    print(x,"/",y)
+elif (opcion == 4 and y == 0):
+    print("El denominador es eigual a cero y")
+    print("No se puede realizar la división.")
+    bandera = True
+elif (opcion == 5):
+    z = pow(x,y)
+    print(x,"^", y)
+elif (opcion == 6 and x > 0):
+    z = log(x)
+    print("Logaritmo de",x)
+elif (opcion == 6 and x <= 0):
+    print("El valor de x es <= a cero y")
+    print("No se puede calcular el logaritmo")
+    bandera = True
+else:
+    print("Opcion no válida")
+    
+# se escribe el resultado con otra condición
+if (opcion < 7 and bandera == False):
+    print("El resultado =", z)
+    
